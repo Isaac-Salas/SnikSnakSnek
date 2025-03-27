4bbba914657279c60f9278861e0f9d45b30b189f
Isaac-Salas <iscprepa@gmail.com>
2025-03-26 20:45:16 -0600

    Momentum kinda good

--- scenes/character_body_3d.gd
+++ scenes/character_body_3d.gd
@@ -1,20 +1,27 @@
 extends CharacterBody3D
 class_name SnakePlayer
 @onready var ANIMATION: AnimatedSprite3D = $AnimatedSprite3D
+@export var max_momentum : float = 20.0
+@export var momentum_step : float = 0.1
 
 var Snakes = 1
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var running = false
-var MOMENTUM = 0.1
+
+@onready var MOMENTUM : float = momentum_step
 
 
 func _physics_process(delta: float) -> void:
+	print(MOMENTUM)
+	if running == false:
+		if MOMENTUM > momentum_step :
+			MOMENTUM -= momentum_step
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	print(running)
+	#print(running)
 	if Input.is_action_pressed("RUN"):
 		running = true
 	else:
@@ -38,15 +45,12 @@ func _physics_process(delta: float) -> void:
 		ANIMATION.play("run")
 		
 		if running ==  true:
-			print(MOMENTUM)
 			velocity.x = direction.x * (SPEED+MOMENTUM)
 			#velocity.z = direction.z * (SPEED+MOMENTUM)
-			if MOMENTUM < 10:
-				MOMENTUM += 0.1
+			if MOMENTUM < max_momentum:
+				MOMENTUM += momentum_step
 				
 		else:
-			if MOMENTUM > 0.1 :
-				MOMENTUM -= 0.1
 				
 			velocity.x = direction.x * (SPEED+MOMENTUM)
 			#velocity.z = direction.z * SPEED+MOMENTUM/2
